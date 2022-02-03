95864a4cdaa445a71601af1c4ebf7992e16a5eb0
CodingWithLaura <laura.ganss@web.de>
2022-02-03 17:20:52 +0100

    Describe main idea of strategy pattern and start project

---
+++ design_pattern/strategy_pattern/src/main.rs
@@ -0,0 +1,5 @@
+//idea is to only define a skeleton of an algorithm, whereas the specific implementation is seperated into different parts
+//client can choose different implementation while workflow where algorithm is used remains the same
+//the genereal algorithm does not depend on specific implementation -> "Dependency Inversion"
+
+fn main() {}
